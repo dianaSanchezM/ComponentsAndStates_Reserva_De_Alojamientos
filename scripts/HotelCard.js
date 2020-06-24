ab1c055d1ee1630c701ac21413c7d3ee9a90f25c
dianaSanchezM <diana.sanchez-m@mail.escuelaing.edu.co>
2020-06-23 23:45:16 -0500

    Some corrections

--- scripts/HotelCard.js
+++ scripts/HotelCard.js
@@ -8,9 +8,9 @@ class HotelCard extends React.Component {
           <img src={hotel.photo}></img>
           <h3>{hotel.name}</h3>
               <p className="info">{hotel.description}</p>
-              <p className="info">{hotel.city}, {hotel.country}</p>
-              <p className="info">{hotel.rooms} Habitaciones</p> 
-              <p className="info">{hotel.price}</p>
+              <p className="info">🌎 {hotel.city}, {hotel.country}</p>
+              <p className="info">🏨{hotel.rooms} Habitaciones</p> 
+              <p className="info">💰{hotel.price}</p>
               
             <button type="button">Reservar</button>
          
